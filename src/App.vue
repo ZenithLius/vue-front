c642a533703b9e873a747cc9f23397d234de9c2a
zdxwbxrkb <zdxwbxrkb109@gmail.com>
2025-12-25 17:04:20 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+
+<template>
+  <h1>You did it!</h1>
+  <ul>
+    <li v-for="item in user" :key="item.id">{{ item.name }}</li>
+  </ul>
+</template>
+
+<script setup>
+import axios from 'axios'
+import { ref } from 'vue'
+const user=ref([])
+const req=()=>{
+   axios.get('http://localhost:3000/api/user').then(
+    (res)=>{
+      user.value=res.data
+    }
+)
+}
+req()
+</script>
+<style scoped></style>
